2971c97b56214bf87b536a57d8c8f61505c4b237
jcastano1997 <jcastano1997@gmail.com>
2024-05-26 22:24:40 -0500

    preserve favorites

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,16 +1,18 @@
 import 'package:english_words/english_words.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../utils/WordPairUtil.dart';
 
 class HomeController extends GetxController {
   final navigationSelectedIndex = 0.obs;
   final pair = WordPair.random().obs;
-  final favorites = <WordPair>[].obs;
+  late final favorites = <WordPair>[].obs;
 
-  @override
-  void onInit() {
-    print('${DateTime.now()}');
-    print("onInit");
-    super.onInit();
+  HomeController() {
+    if (GetStorage().hasData('favorites')) {
+      favorites.value = WordPairUtil.fromJson(GetStorage().read('favorites'));
+    }
   }
 
   void getNext() {
@@ -24,11 +26,13 @@ class HomeController extends GetxController {
     } else {
       favorites.add(pair.value);
     }
+    GetStorage().write('favorites', WordPairUtil.toJson(favorites));
   }
 
   void removeFavorite(WordPair pair) {
     if (favorites.contains(pair)) {
       favorites.remove(pair);
+      GetStorage().write('favorites', WordPairUtil.toJson(favorites));
     }
   }
 
